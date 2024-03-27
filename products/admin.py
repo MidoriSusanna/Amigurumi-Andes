eef1a816f96f2292cee671d156de7e1400d7a4b3
Susanna Legnani <susi@MacBook-Air.station>
2024-03-27 11:39:27 +0100

    Customize admin view

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import Product, Category
 
+class ProductAdmin(admin.ModelAdmin):
+    """"To improve admin experiencea and display products in a certain way"""
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'size',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 admin.site.register(Product)
 admin.site.register(Category)
 
