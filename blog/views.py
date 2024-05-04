eb4d70f0c108966179544cf4037527c647a9cda2
Susanna Legnani <susi@MacBook-Air.station>
2024-05-05 00:42:48 +0200

    Create Post view, post detail and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    """Render a list of published posts"""
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog.html'
+    paginate_by = 8
+
+
+class PostDetail(View):
+    """Render details of a specific post"""
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+            },
+        )
