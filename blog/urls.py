919bf893c63d3b61516ce41330ba7be3174054da
Susanna Legnani <susi@MacBook-Air.station>
2024-05-05 00:49:28 +0200

    Fix mistake in the blog urls.py file

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PostList, PostDetail
 
 urlpatterns = [
-    path('', PostList.as_view(), name='post-list'),
+    path('', PostList.as_view(), name='blog'),
     path('post/<slug:slug>/', PostDetail.as_view(), name='post-detail'),
 ]
