286dffc546ab53fcb64c02d389512e0a4175ef7d
Susanna Legnani <susi@MacBook-Air.station>
2024-05-05 02:05:06 +0200

    Update admin.py to allow superuser to approve posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib import messages
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
@@ -9,16 +10,27 @@ class PostAdmin(admin.ModelAdmin):
     actions = ['publish_posts', 'draft_posts']
 
     def publish_posts(self, request, queryset):
-        queryset.update(status=1)
+        """ Check if the user is a superuser """
+        if request.user.is_superuser:
+            queryset.update(status=1)
+            messages.success(request, f"Selected posts have been marked as published.")
+        else:
+            messages.error(request, "You do not have permission to perform this action.")
     publish_posts.short_description = "Mark selected posts as Published"
 
     def draft_posts(self, request, queryset):
-        queryset.update(status=0)
+        """ Check if the user is a superuser """
+        if request.user.is_superuser:
+            queryset.update(status=0)
+            messages.success(request, f"Selected posts have been marked as draft.")
+        else:
+            messages.error(request, "You do not have permission to perform this action.")
     draft_posts.short_description = "Mark selected posts as Draft"
 
     def save_model(self, request, obj, form, change):
-        obj.author = request.user
+        """ Automatically set the author of the post to the current user """
+        if not obj.pk:  # Check if this is a new object being created
+            obj.author = request.user
         super().save_model(request, obj, form, change)
 
 admin.site.register(Post, PostAdmin)
-
--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from .views import PostList, PostDetail
 urlpatterns = [
     path('', PostList.as_view(), name='blog'),
     path('post/<slug:slug>/', PostDetail.as_view(), name='post_detail'),
+    path('edit/<int:pk>/', PostUpdate.as_view(), name='post_edit'),
+    path('delete/<int:pk>/', PostDelete.as_view(), name='post_delete'),
+    path('approve/<int:pk>/', approve_post, name='post_approve'),
 ]
