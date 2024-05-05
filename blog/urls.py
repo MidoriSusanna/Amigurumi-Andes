1e2cfe45e714ef25ccafa48e700804e5044ae289
Susanna Legnani <susi@MacBook-Air.station>
2024-05-05 11:55:59 +0200

    Add blog post funcionality implemented

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .views import PostList, PostDetail, PostCreate, PostUpdate, PostDelete
 urlpatterns = [
     path('', PostList.as_view(), name='blog'),
     path('post/<slug:slug>/', PostDetail.as_view(), name='post_detail'),
-    path('new/<slug:slug>/', PostCreate.as_view(), name='post_form'),
+    path('new/', PostCreate.as_view(), name='post_form'),
     path('edit/<int:pk>/', PostUpdate.as_view(), name='post_edit'),
     path('delete/<int:pk>/', PostDelete.as_view(), name='post_delete'),
 ]
