996fe307d1a799b4b6479926fd03a7ec0b55d68e
Susanna Legnani <susi@MacBook-Air.station>
2024-05-05 02:10:34 +0200

    Update admin.py to allow superuser approval

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, PostCreate, PostUpdate, PostDelete
 
 urlpatterns = [
     path('', PostList.as_view(), name='blog'),
     path('post/<slug:slug>/', PostDetail.as_view(), name='post_detail'),
+    path('new/<slug:slug>/', PostCreate.as_view(), name='post_form'),
     path('edit/<int:pk>/', PostUpdate.as_view(), name='post_edit'),
     path('delete/<int:pk>/', PostDelete.as_view(), name='post_delete'),
-    path('approve/<int:pk>/', approve_post, name='post_approve'),
 ]
