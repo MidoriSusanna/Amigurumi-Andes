051483080de0ba4169ab55cf92e69d544ad60d5b
Susanna Legnani <susi@MacBook-Air.station>
2024-05-06 14:09:52 +0200

    Add views to join and leave an event in events app

--- events/views.py
+++ events/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Event, EventParticipation
+from .forms import EventForm, ParticipationForm
 
-# Create your views here.
+def event_list(request):
+    """Display list of events"""
+    events = Event.objects.all()
+    return render(request, 'events/event_list.html', {'events': events})
+
+@login_required
+def join_event(request):
+    """Only authenticated users can join events, if they join they are
+    redirected to my events page"""
+    if request.method == 'POST':
+        form = ParticipationForm(request.POST)
+        if form.is_valid():
+            event_id = form.cleaned_data['event_id']
+            event = Event.objects.get(id=event_id)
+            EventJoin.objects.create(user=request.user, event=event)
+            return redirect('my_events')
+    return redirect('events')
+
+@login_required
+def leave_event(request, event_id):
+    """Authneticated users can leave the events they have previously
+    joined"""
+    EventJoin.objects.filter(user=request.user, event_id=event_id).delete()
+    return redirect('my_events')
+
+@login_required
+def my_events(request):
+    """Shows the events the user has joined"""
+    participations = request.user.participations.all()
+    return render(request, 'events/my_events.html', {'participations': participations})
