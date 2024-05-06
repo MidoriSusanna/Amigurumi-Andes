73f6e747cafee18bfd0eaf6944c9f6b33bed7861
Susanna Legnani <susi@MacBook-Air.station>
2024-05-06 13:32:22 +0200

    Events model - admin panel

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Event, EventJoin
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['name', 'location', 'date_time', 'description']
+
+@admin.register(EventJoin)
+class EventJoinAdmin(admin.ModelAdmin):
+    """ Shows in the admin panel which user is participating in which 
+    event directly in the admin interface """
+    list_display = ['user', 'event']
