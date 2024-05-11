26c025b67f3f1fc0966c215e9cb8394c7a14f2a0
Susanna Legnani <susi@MacBook-Air.station>
2024-05-11 16:11:19 +0200

    Add CRUD funcionality to faqs app and fix AWS settings

--- faqs/urls.py
+++ faqs/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('faqs/', views.faq_list, name='faq_list'),
+    path('faqs/edit/<int:pk>/', views.edit_faq, name='edit_faq'),
+    path('faqs/delete/<int:pk>/', views.delete_faq, name='delete_faq'),
 ]
+
--- faqs/views.py
+++ faqs/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import FAQ
+from .forms import FAQForm
 
 def faq_list(request):
-    faqs = FAQ.objects.all()  # Retrieve all FAQ entries from the database
+    faqs = FAQ.objects.all()
     return render(request, 'faqs/faq_list.html', {'faqs': faqs})
+
+@login_required
+def edit_faq(request, pk):
+    faq = get_object_or_404(FAQ, pk=pk)
+    if request.user.is_superuser:
+        if request.method == "POST":
+            form = FAQForm(request.POST, instance=faq)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'FAQ updated successfully.')
+                return redirect('faq_list')
+        else:
+            form = FAQForm(instance=faq)
+        return render(request, 'faqs/edit_faq.html', {'form': form})
+    else:
+        messages.error(request, 'You are not authorized to edit FAQs.')
+        return redirect('faq_list')
+
+@login_required
+def delete_faq(request, pk):
+    faq = get_object_or_404(FAQ, pk=pk)
+    if request.user.is_superuser:
+        if request.method == "POST":
+            faq.delete()
+            messages.success(request, 'FAQ deleted successfully.')
+            return redirect('faq_list')
+        return render(request, 'faqs/delete_faq.html', {'faq': faq})
+    else:
+        messages.error(request, 'You are not authorized to delete FAQs.')
+        return redirect('faq_list')
+
