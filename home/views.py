66f8055fa0c47b1cae0190af453afeaed7696315
Susanna Legnani <susi@MacBook-Air.station>
2024-05-12 23:41:02 +0200

    Add contact us template,view and url

--- home/views.py
+++ home/views.py
@@ -9,3 +9,9 @@ def about(request):
     """A view to return the about page"""
 
     return render(request, 'home/about.html')
+
+def contact(request):
+    """A view to return the contact page"""
+
+    return render(request, 'home/contact.html')
+
