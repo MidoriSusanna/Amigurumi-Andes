a8da82c4a861ea2258307c7ddada85d12a742ef4
Susanna Legnani <susi@MacBook-Air.station>
2024-05-12 10:37:12 +0200

    Fix some css

--- amigurumi_andes/settings.py
+++ amigurumi_andes/settings.py
@@ -176,7 +176,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #    AWS_S3_OBJECT_PARAMETERS = {
 #        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
 #        'CacheControl': 'max-age=94608000',
-    }
+#    }
 
     # Bucket Config
 #    AWS_STORAGE_BUCKET_NAME = 'amigurumi-andes'
