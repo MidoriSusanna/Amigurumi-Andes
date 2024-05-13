6350f8a26f49b5305bdc1d40d38d1692154a8fd7
Susanna Legnani <susi@MacBook-Air.local>
2024-05-13 12:44:25 +0200

    Adjust media path

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
-    featured_image = models.ImageField(upload_to='images/', default='images/placeholder.png')
+    featured_image = models.ImageField(upload_to='media/', default='media/placeholder.png')
     excerpt = models.TextField(blank=True)
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
