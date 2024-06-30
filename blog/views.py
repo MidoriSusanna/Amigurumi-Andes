ac1845eef65e9b6a4c177439ca5994333a0cbbda
Susanna Legnani <susi@MacBook-Air.station>
2024-06-30 13:00:30 +0200

    Fix delete and edit view for the blog app to fix access issue

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import UserPassesTestMixin
 from .models import Post
 from .forms import PostForm
 
@@ -38,22 +39,18 @@ class PostCreate(CreateView):
             form.instance.status = 0  # Draft if not superuser
         response = super().form_valid(form)
         if form.instance.status == 1:
-            messages.success(self.request,
-                             "You have successfully published the blog post.")
+            messages.success(self.request, "You have successfully published the blog post.")
         elif form.instance.status == 0:
-            messages.success(self.request,
-                             "Your blog post is set to draft.")
+            messages.success(self.request, "Your blog post is set to draft.")
         else:
-            messages.success(self.request,
-                             "You have created your blog post."
-                             "It is awaiting approval.")
+            messages.success(self.request, "You have created your blog post. It is awaiting approval.")
         return response
 
 
 @method_decorator(login_required, name='dispatch')
-class PostUpdate(UpdateView):
+class PostUpdate(UserPassesTestMixin, UpdateView):
     """Provides a form to update an existing post.
-    Only accessible to logged-in users."""
+    Only accessible to the author or admin."""
     model = Post
     form_class = PostForm
     template_name = 'blog/post_edit.html'
@@ -62,27 +59,31 @@ class PostUpdate(UpdateView):
     def form_valid(self, form):
         response = super().form_valid(form)
         if form.instance.status == 1:
-            messages.success(self.request,
-                             "You have updated the blog post to published.")
+            messages.success(self.request, "You have updated the blog post to published.")
         elif form.instance.status == 0:
-            messages.success(self.request,
-                             "Your blog post update is set to draft.")
+            messages.success(self.request, "Your blog post update is set to draft.")
         else:
-            messages.success(self.request,
-                             "You have updated your blog post.")
+            messages.success(self.request, "You have updated your blog post.")
         return response
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author or self.request.user.is_superuser
 
 
 @method_decorator(login_required, name='dispatch')
-class PostDelete(DeleteView):
-    """Allows a user to delete a post. Only accessible to logged-in users."""
+class PostDelete(UserPassesTestMixin, DeleteView):
+    """Allows a user to delete a post.
+    Only accessible to the author or admin."""
     model = Post
     template_name = 'blog/post_delete.html'
     success_url = reverse_lazy('blog')
 
     def delete(self, request, *args, **kwargs):
         response = super().delete(request, *args, **kwargs)
-        messages.success(request,
-                         "You have successfully deleted your blog post.")
+        messages.success(request, "You have successfully deleted your blog post.")
         return response
 
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author or self.request.user.is_superuser
