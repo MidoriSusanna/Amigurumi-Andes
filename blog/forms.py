14e5ceed8c887f667f692fc7425acc2e7196e4f6
Susanna Legnani <susi@MacBook-Air.station>
2024-07-07 12:27:33 +0200

    Fix responsiveness of post_form page

--- blog/forms.py
+++ blog/forms.py
@@ -2,14 +2,17 @@ from django import forms
 from .models import Post
 from django_summernote.widgets import SummernoteWidget
 
-
 class PostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user', None)
         super(PostForm, self).__init__(*args, **kwargs)
         # Only superusers can modify the 'status' field
         if user and not user.is_superuser:
-            self.fields.pop('status', None)  # Hide 'status' fromnon-superusers
+            self.fields.pop('status', None)  # Hide 'status' from non-superusers
+
+        # Add Bootstrap classes to all fields
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
 
     class Meta:
         model = Post
@@ -17,6 +20,5 @@ class PostForm(forms.ModelForm):
             'title', 'slug', 'featured_image', 'excerpt', 'content', 'status'
         ]
         widgets = {
-            'content': SummernoteWidget()  # Enables rich text editing
+            'content': SummernoteWidget(),  # Enables rich text editing
         }
-
